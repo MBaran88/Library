7dd39aca6afa9e75cd8042dfa0f66a45d599e9ae
MBaran88 <zdzibq@gmail.com>
2017-04-27 19:14:41 +0200

    library

---
+++ src/com/company/Book.java
@@ -0,0 +1,8 @@
+package com.company;
+
+/**
+ * Created by RENT on 2017-04-27.
+ */
+public class Book {
+    (String title) {this.title = title;}
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,17 +4,24 @@ import java.awt.print.Book;
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        class Library{
+        Book book1 = new Book(title:"Pan Tadeusz");
+        Book book2 = new Book(title:"Matrix");
+        Book book3 = new Book(title:"Ogniem i Mieczem");
 
-             List<Book> books= new ArrayList<>();
+        List<Book> booksForLibrary = new ArrayList<>();
+        booksForLibrary.add(book1);
+        booksForLibrary.add(book2);
+        booksForLibrary.add(book3);
 
-             Library(){
+        Library library = new Library();
 
-             }}
-
-         }
+        //Library.addBook(book1);
+        //Library.addBook(book2);
+        //Library.addBook(book3);
+        System.out.println(library.allBooks.size());
     }
+}
