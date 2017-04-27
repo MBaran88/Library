a8d5d6e110d1d2a21e39cc22b21a349a29f14a0a
MBaran88 <zdzibq@gmail.com>
2017-04-27 18:01:24 +0200

    library

---
+++ src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.awt.print.Book;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        class Library{
+
+             private Book book;
+             private Integer[] myInts = new Integer[10];
+             List<Book> books= new ArrayList<>();
+
+             Library(){
+
+             }}
+
+         }
+    }
